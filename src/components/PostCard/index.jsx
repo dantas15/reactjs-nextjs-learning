6663268e53bcde91a99de43d03bb27eab9e72807
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-07-25 10:27:28 -0300

    Props, organização dos componentes e paginação

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+const PostCard = ({cover, title, id, body}) =>  (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  </div>
+  )
+
+
+export { PostCard };

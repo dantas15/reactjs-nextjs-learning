4fcefc43e365e7deb6ad6c6d43a1128cb09a80c0
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-07-30 14:50:37 -0300

    Counter com Componentes de Classe

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,74 +1,28 @@
-// import logo from "./logo.svg";
-import { useEffect, useState, useCallback } from "react";
-
+import { Component } from "react";
 import "./styles.css";
 
-import { Posts } from "../../components/Posts";
-import { loadPosts } from "../../utils/load-posts";
-import { Button } from "../../components/Button";
-import { TextInput } from "../../components/TextInput";
-
-export const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postsPerPage] = useState(10);
-  const [searchValue, setSearchValue] = useState("");
-
-  const filteredPosts = !!searchValue
-    ? allPosts.filter((post) => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-    : posts;
-
-  const noMorePosts = page + postsPerPage >= allPosts.length;
-
-  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
-    const postsAndPhotos = await loadPosts();
-
-    setPosts(postsAndPhotos.slice(page, postsPerPage));
-    setAllPosts(postsAndPhotos);
-  }, []);
-
-  useEffect(() => {
-    console.log(new Date().toLocaleDateString("pt-br"));
-    handleLoadPosts(0, postsPerPage);
-  }, [handleLoadPosts, postsPerPage]);
-
-  const loadMorePosts = async () => {
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    posts.push(...nextPosts);
-
-    setPosts(posts);
-    setPage(nextPage);
+export class Home extends Component {
+  state = {
+    counter: 0,
   };
 
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
+  handleClick = () => {
+    this.setState(
+      (prevState, prevProps) => {
+        return { counter: prevState.counter + prevProps.numberToIncrement };
+      },
+      () => {
+        console.log(this.state.counter);
+      }
+    );
   };
 
-  return (
-    <section className="container">
-      <div className="search-container">
-        {!!searchValue && <h1>Search value: {searchValue}</h1>}
-        <TextInput searchValue={searchValue} handleChange={handleChange} />
+  render() {
+    return (
+      <div className="container">
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleClick}>Increment</button>
       </div>
-
-      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-
-      {filteredPosts.length === 0 && <p>Não existem posts :(</p>}
-
-      <div className="button-container">
-        {!searchValue && (
-          <Button
-            text="Load more posts"
-            onClick={loadMorePosts}
-            disabled={noMorePosts}
-          />
-        )}
-      </div>
-    </section>
-  );
-};
+    );
+  }
+}
